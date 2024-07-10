c8daab34d94d633c8835b35560399fbe7d4ca0c1
dohun <shine_17@naver.com>
2024-07-10 09:34:06 +0900

    07-10

--- src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -10,9 +10,7 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 public class BeanDefinitionTest {
 
 //    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
-
     GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
-
 
     @Test
     @DisplayName("빈 설정 메타정보 확인")
